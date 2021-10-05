7cae91be4a0e79dfb9a7a8f25ebb8a5e41d14d1e
sigitfitriono <91943720+sigitfitriono@users.noreply.github.com>
2021-10-05 15:50:18 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // Run when the webpage is loaded
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Apakah Anda ingin memutar musiknya??',
+    title: 'Ingin memutar musiknya??',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
