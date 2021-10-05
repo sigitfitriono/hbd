ebc04d8314b1feb27842670be0cd0d3b6991a2ca
sigitfitriono <91943720+sigitfitriono@users.noreply.github.com>
2021-10-05 15:49:09 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,14 +1,14 @@
 // Run when the webpage is loaded
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Are you sure to play music in background?',
+    title: 'Apakah Anda ingin memutar musiknya??',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#3085d6',
     cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes',
-    cancelButtonText: 'No',
+    confirmButtonText: 'Ya',
+    cancelButtonText: 'Tidak',
   }).then((result) => {
     if (result.isConfirmed) {
       document.querySelector('.song').play();
